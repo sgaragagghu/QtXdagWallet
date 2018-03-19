146c2f88b8c8ad7f8da023e30a60994192acc1a5
Bill Levesque <nongjihua947@pingan.com.cn>
2018-03-19 18:30:11 +0800

    1.translate

--- qt/QtXdagWallet/resource/language/chinese.ts
+++ qt/QtXdagWallet/resource/language/chinese.ts
@@ -2,11 +2,91 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="zh_CN">
 <context>
+    <name>ErrorDialog</name>
+    <message>
+        <location filename="errordialog.ui" line="13"/>
+        <source>Dialog</source>
+        <translation>窗口</translation>
+    </message>
+    <message>
+        <location filename="ErrorDialog.cpp" line="15"/>
+        <source>OK</source>
+        <translation>确定</translation>
+    </message>
+    <message>
+        <location filename="ErrorDialog.cpp" line="39"/>
+        <source>password error</source>
+        <translation>密码错误</translation>
+    </message>
+    <message>
+        <location filename="ErrorDialog.cpp" line="41"/>
+        <source>password foramt error 
+ length 8-18 consist at leat 1 charactor 1 number</source>
+        <translation>密码格式错误，长度8-18位，至少一个数字一个字母</translation>
+    </message>
+    <message>
+        <location filename="ErrorDialog.cpp" line="43"/>
+        <source>password not the same</source>
+        <translation>两次密码输入不一致</translation>
+    </message>
+    <message>
+        <location filename="ErrorDialog.cpp" line="45"/>
+        <source>nothing transfer</source>
+        <translation>余额不足</translation>
+    </message>
+    <message>
+        <location filename="ErrorDialog.cpp" line="47"/>
+        <source>balance too small</source>
+        <translation>发送额度过小</translation>
+    </message>
+    <message>
+        <location filename="ErrorDialog.cpp" line="49"/>
+        <source>invalid receive address</source>
+        <translation>接受地址非法</translation>
+    </message>
+    <message>
+        <location filename="ErrorDialog.cpp" line="51"/>
+        <location filename="ErrorDialog.cpp" line="53"/>
+        <source>unkown error</source>
+        <translation>未知错误</translation>
+    </message>
+</context>
+<context>
     <name>PwdDialog</name>
     <message>
         <location filename="pwddialog.ui" line="14"/>
         <source>Dialog</source>
         <translation>窗口</translation>
+    </message>
+    <message>
+        <location filename="PwdDialog.cpp" line="15"/>
+        <source>OK</source>
+        <translation>确认</translation>
+    </message>
+    <message>
+        <location filename="PwdDialog.cpp" line="40"/>
+        <source>Dagger Wallet(XDAG)</source>
+        <translation>Xdag钱包</translation>
+    </message>
+    <message>
+        <location filename="PwdDialog.cpp" line="45"/>
+        <source>input password</source>
+        <translation>输入密码</translation>
+    </message>
+    <message>
+        <location filename="PwdDialog.cpp" line="48"/>
+        <source>set password</source>
+        <translation>设置密码</translation>
+    </message>
+    <message>
+        <location filename="PwdDialog.cpp" line="51"/>
+        <source>confirm password</source>
+        <translation>确认密码</translation>
+    </message>
+    <message>
+        <location filename="PwdDialog.cpp" line="54"/>
+        <source>set random keys</source>
+        <translation>设置随机串</translation>
     </message>
 </context>
 <context>
@@ -43,6 +123,8 @@
     <message>
         <location filename="QtWalletMain.cpp" line="70"/>
         <location filename="QtWalletMain.cpp" line="72"/>
+        <location filename="QtWalletMain.cpp" line="381"/>
+        <location filename="QtWalletMain.cpp" line="387"/>
         <source>Not Ready</source>
         <translation>未就绪</translation>
     </message>
@@ -74,6 +156,97 @@
         <location filename="QtWalletMain.cpp" line="193"/>
         <source>Language</source>
         <translation>语言</translation>
+    </message>
+    <message>
+        <location filename="QtWalletMain.cpp" line="410"/>
+        <location filename="QtWalletMain.cpp" line="447"/>
+        <source>Initializing.</source>
+        <translation>初始化中.</translation>
+    </message>
+    <message>
+        <location filename="QtWalletMain.cpp" line="412"/>
+        <source>Generating keys...</source>
+        <translation>创建密钥...</translation>
+    </message>
+    <message>
+        <location filename="QtWalletMain.cpp" line="414"/>
+        <source>The local storage is corrupted. Resetting blocks engine.</source>
+        <translation>本地区块损坏.重置区块引擎中.</translation>
+    </message>
+    <message>
+        <location filename="QtWalletMain.cpp" line="416"/>
+        <source>Loading blocks from the local storage.</source>
+        <translation>从本地读取区块中.</translation>
+    </message>
+    <message>
+        <location filename="QtWalletMain.cpp" line="418"/>
+        <source>Blocks loaded. Waiting for &apos;run&apos; command.</source>
+        <translation>区块已读取.等待运行命令.</translation>
+    </message>
+    <message>
+        <location filename="QtWalletMain.cpp" line="420"/>
+        <source>Trying to connect to the test network.</source>
+        <translation>尝试连接到测试网络.</translation>
+    </message>
+    <message>
+        <location filename="QtWalletMain.cpp" line="422"/>
+        <source>Trying to connect to the main network.</source>
+        <translation>尝试连接到主干网络中.</translation>
+    </message>
+    <message>
+        <location filename="QtWalletMain.cpp" line="424"/>
+        <source>Trying to connect to the testnet pool.</source>
+        <translation>尝试连接到测试矿池.</translation>
+    </message>
+    <message>
+        <location filename="QtWalletMain.cpp" line="426"/>
+        <source>Trying to connect to the mainnet pool.</source>
+        <translation>尝试连接到主网矿池.</translation>
+    </message>
+    <message>
+        <location filename="QtWalletMain.cpp" line="428"/>
+        <source>Connected to the test network. Synchronizing.</source>
+        <translation>已经连接到测试网络.同步中.</translation>
+    </message>
+    <message>
+        <location filename="QtWalletMain.cpp" line="430"/>
+        <source>Connected to the main network. Synchronizing.</source>
+        <translation>已经连接到主网.同步中.</translation>
+    </message>
+    <message>
+        <location filename="QtWalletMain.cpp" line="432"/>
+        <source>Waiting for transfer to complete.</source>
+        <translation>等待发币完成.</translation>
+    </message>
+    <message>
+        <location filename="QtWalletMain.cpp" line="434"/>
+        <source>Connected to the testnet pool. No mining.</source>
+        <translation>已经连接到测试矿池.未开始挖矿.</translation>
+    </message>
+    <message>
+        <location filename="QtWalletMain.cpp" line="436"/>
+        <source>Connected to the mainnet pool. No mining.</source>
+        <translation>已经连接到主网矿池.未挖矿.</translation>
+    </message>
+    <message>
+        <location filename="QtWalletMain.cpp" line="438"/>
+        <source>Connected to the testnet pool. Mining on. Normal testing.</source>
+        <translation>已经连接到测试矿池.挖矿中.正常测试中.</translation>
+    </message>
+    <message>
+        <location filename="QtWalletMain.cpp" line="440"/>
+        <source>Connected to the mainnet pool. Mining on. Normal operation.</source>
+        <translation>已经连接到主网矿池.挖矿中.正常操作.</translation>
+    </message>
+    <message>
+        <location filename="QtWalletMain.cpp" line="442"/>
+        <source>Synchronized with the test network. Normal testing.</source>
+        <translation>已经跟测试网络同步.正常测试中.</translation>
+    </message>
+    <message>
+        <location filename="QtWalletMain.cpp" line="444"/>
+        <source>Synchronized with the main network. Normal operation.</source>
+        <translation>已经跟主网络同步.正常操作.</translation>
     </message>
 </context>
 <context>
--- qt/QtXdagWallet/resource/language/germany.ts
+++ qt/QtXdagWallet/resource/language/germany.ts
@@ -2,11 +2,91 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="de_DE">
 <context>
+    <name>ErrorDialog</name>
+    <message>
+        <location filename="errordialog.ui" line="13"/>
+        <source>Dialog</source>
+        <translation type="unfinished">dialog</translation>
+    </message>
+    <message>
+        <location filename="ErrorDialog.cpp" line="15"/>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ErrorDialog.cpp" line="39"/>
+        <source>password error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ErrorDialog.cpp" line="41"/>
+        <source>password foramt error 
+ length 8-18 consist at leat 1 charactor 1 number</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ErrorDialog.cpp" line="43"/>
+        <source>password not the same</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ErrorDialog.cpp" line="45"/>
+        <source>nothing transfer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ErrorDialog.cpp" line="47"/>
+        <source>balance too small</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ErrorDialog.cpp" line="49"/>
+        <source>invalid receive address</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ErrorDialog.cpp" line="51"/>
+        <location filename="ErrorDialog.cpp" line="53"/>
+        <source>unkown error</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>PwdDialog</name>
     <message>
         <location filename="pwddialog.ui" line="14"/>
         <source>Dialog</source>
         <translation>dialog</translation>
+    </message>
+    <message>
+        <location filename="PwdDialog.cpp" line="15"/>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="PwdDialog.cpp" line="40"/>
+        <source>Dagger Wallet(XDAG)</source>
+        <translation type="unfinished">Xdag Brieftasche</translation>
+    </message>
+    <message>
+        <location filename="PwdDialog.cpp" line="45"/>
+        <source>input password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="PwdDialog.cpp" line="48"/>
+        <source>set password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="PwdDialog.cpp" line="51"/>
+        <source>confirm password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="PwdDialog.cpp" line="54"/>
+        <source>set random keys</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -18,62 +98,155 @@
     </message>
     <message>
         <location filename="QtWalletMain.cpp" line="50"/>
-        <location filename="QtWalletMain.cpp" line="159"/>
+        <location filename="QtWalletMain.cpp" line="183"/>
         <source>Dagger Wallet(XDAG)</source>
         <translation>Xdag Brieftasche</translation>
     </message>
     <message>
-        <location filename="QtWalletMain.cpp" line="54"/>
-        <location filename="QtWalletMain.cpp" line="160"/>
+        <location filename="QtWalletMain.cpp" line="53"/>
+        <location filename="QtWalletMain.cpp" line="184"/>
         <source>Pool</source>
         <translation>Pool</translation>
     </message>
     <message>
-        <location filename="QtWalletMain.cpp" line="56"/>
-        <location filename="QtWalletMain.cpp" line="161"/>
+        <location filename="QtWalletMain.cpp" line="55"/>
+        <location filename="QtWalletMain.cpp" line="185"/>
         <source>Connect</source>
         <translation>Connect</translation>
     </message>
     <message>
-        <location filename="QtWalletMain.cpp" line="70"/>
-        <location filename="QtWalletMain.cpp" line="163"/>
+        <location filename="QtWalletMain.cpp" line="69"/>
+        <location filename="QtWalletMain.cpp" line="187"/>
         <source>Balance</source>
         <translation>gleichgewicht</translation>
     </message>
     <message>
-        <location filename="QtWalletMain.cpp" line="71"/>
-        <location filename="QtWalletMain.cpp" line="73"/>
+        <location filename="QtWalletMain.cpp" line="70"/>
+        <location filename="QtWalletMain.cpp" line="72"/>
+        <location filename="QtWalletMain.cpp" line="381"/>
+        <location filename="QtWalletMain.cpp" line="387"/>
         <source>Not Ready</source>
         <translation>nicht bereit</translation>
     </message>
     <message>
-        <location filename="QtWalletMain.cpp" line="72"/>
-        <location filename="QtWalletMain.cpp" line="164"/>
+        <location filename="QtWalletMain.cpp" line="71"/>
+        <location filename="QtWalletMain.cpp" line="188"/>
         <source>Account</source>
         <translation>Konto</translation>
     </message>
     <message>
-        <location filename="QtWalletMain.cpp" line="91"/>
-        <location filename="QtWalletMain.cpp" line="166"/>
+        <location filename="QtWalletMain.cpp" line="90"/>
+        <location filename="QtWalletMain.cpp" line="190"/>
         <source>Transfer</source>
         <translation>Transfer</translation>
     </message>
     <message>
-        <location filename="QtWalletMain.cpp" line="93"/>
-        <location filename="QtWalletMain.cpp" line="167"/>
+        <location filename="QtWalletMain.cpp" line="92"/>
+        <location filename="QtWalletMain.cpp" line="191"/>
         <source>To</source>
         <translation>Auf</translation>
     </message>
     <message>
-        <location filename="QtWalletMain.cpp" line="95"/>
-        <location filename="QtWalletMain.cpp" line="168"/>
+        <location filename="QtWalletMain.cpp" line="94"/>
+        <location filename="QtWalletMain.cpp" line="192"/>
         <source>Send</source>
         <translation>Schick</translation>
     </message>
     <message>
-        <location filename="QtWalletMain.cpp" line="169"/>
+        <location filename="QtWalletMain.cpp" line="193"/>
         <source>Language</source>
         <translation>Sprache</translation>
+    </message>
+    <message>
+        <location filename="QtWalletMain.cpp" line="410"/>
+        <location filename="QtWalletMain.cpp" line="447"/>
+        <source>Initializing.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QtWalletMain.cpp" line="412"/>
+        <source>Generating keys...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QtWalletMain.cpp" line="414"/>
+        <source>The local storage is corrupted. Resetting blocks engine.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QtWalletMain.cpp" line="416"/>
+        <source>Loading blocks from the local storage.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QtWalletMain.cpp" line="418"/>
+        <source>Blocks loaded. Waiting for &apos;run&apos; command.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QtWalletMain.cpp" line="420"/>
+        <source>Trying to connect to the test network.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QtWalletMain.cpp" line="422"/>
+        <source>Trying to connect to the main network.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QtWalletMain.cpp" line="424"/>
+        <source>Trying to connect to the testnet pool.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QtWalletMain.cpp" line="426"/>
+        <source>Trying to connect to the mainnet pool.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QtWalletMain.cpp" line="428"/>
+        <source>Connected to the test network. Synchronizing.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QtWalletMain.cpp" line="430"/>
+        <source>Connected to the main network. Synchronizing.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QtWalletMain.cpp" line="432"/>
+        <source>Waiting for transfer to complete.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QtWalletMain.cpp" line="434"/>
+        <source>Connected to the testnet pool. No mining.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QtWalletMain.cpp" line="436"/>
+        <source>Connected to the mainnet pool. No mining.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QtWalletMain.cpp" line="438"/>
+        <source>Connected to the testnet pool. Mining on. Normal testing.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QtWalletMain.cpp" line="440"/>
+        <source>Connected to the mainnet pool. Mining on. Normal operation.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QtWalletMain.cpp" line="442"/>
+        <source>Synchronized with the test network. Normal testing.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="QtWalletMain.cpp" line="444"/>
+        <source>Synchronized with the main network. Normal operation.</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
